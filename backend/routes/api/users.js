2ed81729d2aca7d45ed5efe9761db03eea2246b0
TechWillie <techwillie5@gmail.com>
2024-12-15 04:44:05 -0500

    Add User input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 //! Sign up
 //  add the POST /api/users route to the router 
@@ -34,5 +36,31 @@ router.post('/',async (req, res) => {
       });
     }
   );
+
+
+// The validateSignup middleware is composed of the check and handleValidationErrors middleware
+  const validateSignup = [
+    check('email')
+    // It checks to see if req.body.email exists and is an emai
+      .exists({ checkFalsy: true })
+      .isEmail()
+      .withMessage('Please provide a valid email.'),
+    check('username')
+    // req.body.username is a minimum length of 4 and 
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+    // is not an emai
+      .not()
+      .isEmail()
+      .withMessage('Username cannot be an email.'),
+    check('password')
+    // is not empty and has a minimum length of 6
+      .exists({ checkFalsy: true })
+      .isLength({ min: 6 })
+      .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+  ];
 
 module.exports = router;
