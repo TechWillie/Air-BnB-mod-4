b71da0fcf413ffcf7fa2348ce9009c9af89c733f
TechWillie <techwillie5@gmail.com>
2024-12-16 03:15:24 -0500

    Add json file for render

---
+++ routes/api/index.js
@@ -0,0 +1,47 @@
+// backend/routes/api/index.js
+const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require('../../utils/auth.js');
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { requireAuth } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+
+
+// // GET /api/set-token-cookie
+// router.get('/set-token-cookie', async (_req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: 'Demo-lition'
+//     }
+//   });
+//   setTokenCookie(res, user);
+//   return res.json({ user: user });
+// });
+            
+    router.use(restoreUser);
+
+    router.use('/session', sessionRouter);
+
+    router.use('/users', usersRouter);
+
+    router.post('/test', function(req, res) {
+        // desturcture the firstname and lastanme from req.body
+        const { firstName, lastName } = req.body;
+
+        res.json({ requestBody: req.body,
+            firstName: firstName || 'Unknown',
+            lastName: lastName || 'Unknown'
+         });
+        });
+            
+            // // GET /api/require-auth
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+module.exports = router;
