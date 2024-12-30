193ce63c00b4f1c0ca2c62ee670a2f183351b5f9
TechWillie <techwillie5@gmail.com>
2024-12-29 23:14:11 -0500

    Delete image for a review complete

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -33,4 +33,33 @@ router.delete('/:imageId', requireAuth, async (req, res) => {
   }
 });
 
+// ! Delete an image for a review
+router.delete('/:imageId', requireAuth, async (req, res) => {
+    const {imageId} = req.params;
+    const userId = req.user.id;
+    try{
+      // if image exist?
+      const image = await ReviewImage.findByPk(imageId);
+      if(!image){return res.status(404).json({message: 'Review image not found'});
+    }
+  
+      // review linked to the image
+      const review = await Review.findByPk(image.reviewId);
+      if(!review){return res.status(404).json({message: 'Review not found'});
+    }
+  
+      // if user is the owner of the review?
+      if(review.userId !== userId){
+        return res.status(403).json({message: 'Forbidden: Not authorized to delete this image'});
+      }
+      // Delete image
+      await image.destroy();
+      // Return success message
+      return res.json({message: 'Review image successfully deleted'});
+    }catch(error){
+      console.error(error);
+      return res.status(500).json({message: 'Internal server error'});
+    }
+  });
+
 module.exports = router;
