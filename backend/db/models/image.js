8408d96721c8990ec1d52b6af5236d3c2b22590f
TechWillie <techwillie5@gmail.com>
2025-01-06 10:19:44 -0500

    took of static relation on Image model

--- backend/db/models/image.js
+++ backend/db/models/image.js
@@ -11,9 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Image.belongsTo(models.Spot, {
-        foreignKey: "ownerId"
-      })
+      // Image.belongsTo(models.Spot, {
+      //   foreignKey: "ownerId"
+      // })
     }
   }
   Image.init({
