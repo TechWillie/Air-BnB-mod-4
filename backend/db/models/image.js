5e1ac853683a37595b423c0362f9e4fa269e30e1
TechWillie <techwillie5@gmail.com>
2025-01-08 01:43:55 -0500

    Push for render.. Modifies Image model

--- backend/db/models/image.js
+++ backend/db/models/image.js
@@ -11,9 +11,13 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      // Image.belongsTo(models.Spot, {
-      //   foreignKey: "ownerId"
-      // })
+      Image.belongsTo(models.Spot, {
+        foreignKey: "ownerId"
+      })
+
+      Image.belongsTo(models.Review, {
+        foreignKey: "userId"
+      })
     }
   }
   Image.init({
