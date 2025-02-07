0755b85d467eb757a1128db92ef42975cde468e9
TechWillie <techwillie5@gmail.com>
2025-02-07 02:04:15 -0500

    logout not complete, troubleshooting

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -86,3 +86,11 @@ export const restoreUser = () => async dispatch => {
   dispatch(setUser(data.user));
   return response;
 };
+
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+        method: 'DELETE'
+    })
+    dispatch(removeUser())
+    return response
+}
