f16c31f15a725d958a34038925c88952d70bbc5d
TechWillie <techwillie5@gmail.com>
2025-02-12 23:10:05 -0500

    spots and details wo pics

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,13 @@
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux' 
 import {thunk} from 'redux-thunk'
 import sessionReducer from './session';
+import spotsReducer from './spots';
 
 
 // Create a rootReducer that calls combineReducers and pass in an empty object for now.
 const rootReducer = combineReducers({
-  session: sessionReducer
+  session: sessionReducer,
+  spots: spotsReducer
 });
 
 // Initialize an enhancer variable 
